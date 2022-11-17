5740bb218c3aac559c4cab971ef9d0a8fd7632ce
Nikolay Chikovani <nchikovany1@gmail.com>
2022-11-17 12:33:12 +0300

    strapi nginx

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -2,15 +2,15 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'postgres'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'mydb'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'root'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
-    settings: {
-      forceMigration: false
-    }
+    // settings: {
+    //   forceMigration: false
+    // }
   },
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import './App.css'
+
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
-    const res = await fetch('/api/posts/values', {method: 'GET'});
+    const res = await fetch('/strapi/api/posts?populate[0]=image', {method: 'GET'});
     const targetPosts = await res.json();
-    setPosts(targetPosts.data)
+    if (!targetPosts.data) return;
+    setPosts(targetPosts.data);
   };
 
   useEffect(() => {
@@ -18,12 +25,28 @@ function App() {
   return (
     <div className="App">
       <h1>strapi-posts</h1>
-      <div>
+      <div className="posts">
         {posts.map(post => (
-          <div className="post" key={post.id}>
-            <span className="post__id">{post.id}</span>
-            <span>{post.title}</span>
-          </div>
+          <>
+            <Card key={post.id} sx={{ width: 392 }} style={{marginBottom: "20px"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={`/strapi${post.attributes.image.data.attributes.url}`}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {post.attributes.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {post.attributes.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </>
         ))}
       </div>
     </div>
