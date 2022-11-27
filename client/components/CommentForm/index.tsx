8d85ad979340daaf43ab007073145368e1d2df53
Nikolay Chikovani <nchikovany1@gmail.com>
2022-11-27 17:25:57 +0300

    comment form

--- client/components/CommentForm/index.tsx
+++ client/components/CommentForm/index.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import {Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {TextField, Typography} from "@mui/material";
 import styles from './style.module.scss';
+import {LoadingButton} from "@mui/lab";
 
 const CommentForm = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+
+  const onSubmit = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      console.log(name, comment);
+      setIsLoading(false);
+    }, 1000);
+  }
 
   return (
     <>
       <Typography variant="h3" className={styles.title}>Оставьте комментарий</Typography>
+      <TextField
+        placeholder="Ваше имя"
+        fullWidth
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles.name}
+      />
+      <TextField
+        placeholder="Ваш комментарий"
+        fullWidth
+        multiline
+        rows={6}
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        className={styles.comment}
+      />
+      <div className={styles.button_wrapper}>
+        <LoadingButton
+          variant="outlined"
+          loading={isLoading}
+          loadingPosition="end"
+          onClick={onSubmit}
+        >Отправить</LoadingButton>
+      </div>
     </>
   );
 };
--- client/styles/muiTheme.ts
+++ client/styles/muiTheme.ts
@@ -17,13 +17,43 @@ const theme = createTheme({
           letterSpacing: "0.05em",
           height: "38px",
           padding: "0 50px",
-          ":hover": {
+          "&:hover": {
             borderColor: Colors.gray11,
             background: Colors.gray16
           }
         }
       },
-    }
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          borderRadius: "0 !important",
+          padding: "0 !important",
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: `1px solid ${Colors.gray13} !important`,
+          },
+          ".MuiInputBase-input": {
+            padding: "12px 20px",
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: `${Colors.gray11} !important`,
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            border: `1px solid ${Colors.gray9} !important`,
+          },
+        },
+        input: {
+          fontSize: "15px",
+          height: "19px",
+          lineHeight: "19px",
+          "&::placeholder": {
+            color: Colors.gray8,
+            opacity: 1,
+            fontSize: '15px'
+          }
+        }
+      }
+    },
   }
 });
 
